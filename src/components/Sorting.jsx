478a080132fed5e8dec3aa01f2f1e793400beb1b
NickFedko <nikitosfedkos@gmail.com>
2023-02-02 17:32:33 +0200

    add categories api

--- src/components/Sorting.jsx
+++ src/components/Sorting.jsx
@@ -1,16 +1,48 @@
+import axios from 'axios'
+import { useState } from 'react';
 import '../assets/styles/Sorting.css'
 
 export default function Sorting() {
+
+    async function categories() {
+        let result = await axios({
+            method: 'GET',
+            url: 'https://demo-api.apiko.academy/api/categories',
+            headers: {
+                'accept': 'application/json'
+            }
+        }) 
+        let categories = localStorage.setItem('category', JSON.stringify(result.data))
+    }
+
+    categories();
+
+    const items = JSON.parse(localStorage.getItem('category'));
+
+    const categoryHandleChange = (id) => {
+        getCategory(id);
+    }
+
+    async function getCategory(id) {
+        let result = await axios ({
+            method: 'GET',
+            url: `https://demo-api.apiko.academy/api/categories/${id}`,
+            headers: {
+                'accept': 'application/json'
+            }
+        })
+        console.log(result)
+    }
+
     return(
         <form className="sorting__form">
             <input className="sorting__form__search" type="search" placeholder="Search products by name"/>
-            <select className="sorting__form__category" placeholder="Choose category">
-                <option>
-                  Sneakers  
-                </option>
-                <option>
-                    Slippers
-                </option>
+            <select className="sorting__form__category" placeholder="Choose category" onChange={(e) => categoryHandleChange(e.target[e.target.selectedIndex].id)}>
+                {items.map(item => (
+                    <option key={item.id} value={item.name} id={item.id}> 
+                        {item.name}
+                    </option>
+                ))}
             </select>
             <select className="sorting__form__sorter" placeholder="Sort by">
                 <option>
