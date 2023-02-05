ad2e07f740d058e98b94b31be44eb34a9c5f0204
Denys <denys.fedko@uployal.io>
2023-02-05 11:28:20 +0200

    temporary disabled Reacr Strict Mode

--- src/index.js
+++ src/index.js
@@ -10,14 +10,10 @@ import store from './store/store';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <Provider store={store}>
-      <React.StrictMode>
+      {/*<React.StrictMode>*/}
         <BrowserRouter>
           <App />
         </BrowserRouter>
-      </React.StrictMode>
+      {/*</React.StrictMode>*/}
     </Provider>
-  );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+);
