a36fe58a9a2875ee440ade4df399a9d94dfdbf50
NickFedko <nikitosfedkos@gmail.com>
2023-02-20 17:24:09 +0200

    add loader

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -2,6 +2,7 @@ import SearchBar from "../components/SearchBar";
 import ProductItem from "../components/ProductItem";
 import { useState, useEffect } from "react";
 import getProducts from "../services/api/products";
+import { ClipLoader } from 'react-spinners';
 
 export default function MainPage() {
     const [products, setProducts] = useState([]); //  rename array
@@ -10,12 +11,15 @@ export default function MainPage() {
     const [idCategory, setIdCategory] = useState(1);
     const category = idCategory ? `/categories/${idCategory}`: '';
     const search =  (searchProducts.length >= 3) ? `/search?keywords=${searchProducts}` : '' 
+    const [loading, setLoading] = useState(false);
     
     useEffect(() => {
+        setLoading(true);
         getProducts(maxLimitOfProducts, category, search).then((response) => {
             setProducts(response.data);
         }).catch(error => console.log(error))
             .finally(() => {
+                setLoading(false)
         })
     }, [maxLimitOfProducts, idCategory, searchProducts, category, search])
 
@@ -30,7 +34,11 @@ export default function MainPage() {
                 />
             ))}
         </div>
-        {products.length===0 ? <h1>Not found</h1> : <button onClick={() => setMaxLimitOfProducts(maxLimitOfProducts + 20)}>Load more</button>}
+        {products.length===0 && searchProducts.length>=3 ? <h1>Not found</h1> : <button className="load_more__button" onClick={() => setMaxLimitOfProducts(maxLimitOfProducts + 20)}>Load more...</button>}
+        {loading &&
+        <div className="loader__overlay">
+            <ClipLoader className="loader_icon" size={200} color={'white'}/>
+        </div>}
     </div>
     )
 }
